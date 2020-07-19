027e50edb693e1563b915d3417269b5ef799d405
zh6335901 <zh6335901@hotmail.com>
2020-07-19 22:08:18 +0800

    Initial commit

---
+++ src/Services/JobConfig/BatchX.JobConfigs.Api/Controllers/JobConfigController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BatchX.JobConfig.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobConfigController : ControllerBase
+    {
+    }
+}
