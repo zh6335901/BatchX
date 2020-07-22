a62966822f5e52be795471f423ea196fc55a5ce7
zh6335901 <zh6335901@hotmail.com>
2020-07-22 21:01:11 +0800

    Add JobConfig model

---
+++ src/Services/JobConfig/BatchX.JobConfigs.Api/Models/IJobConfigRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace BatchX.JobConfig.Api.Models
+{
+    public interface IJobConfigRepository
+    {
+        Task<JobConfig> FindById(string id);
+
+        Task Add(JobConfig jobConfig);
+
+        Task Remove(string Id);
+
+        Task Update(JobConfig config);
+    }
+}
---
+++ src/Services/JobConfig/BatchX.JobConfigs.Api/Models/JobConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BatchX.JobConfig.Api.Models
+{
+    public class JobConfig
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTimeOffset CreationDate { get; set; }
+
+        public DateTimeOffset UpdateDate { get; set; }
+    }
+}
